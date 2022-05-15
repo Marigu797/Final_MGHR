daaf61f7cc6441eccb1bb1ce2413a5b499ff37c8
Mariana Gutiérrez Rivero <marigurc@gmail.com>
2022-05-14 22:39:28 -0300

    logoAp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentsComponent } from './components/components.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentsComponent,
+    HeaderComponent,
+    LogoAPComponent
   ],
   imports: [
     BrowserModule,
